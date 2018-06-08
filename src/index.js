4274b0a62914cdccc65b1ce1a817e032d3274397
Marty Stepien <marty.stepien@gmail.com>
2018-06-08 19:16:43 +0100

    redux added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './App';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+	render() {
+		return <div> Hello </div>
+	}
+}
+
+class Goodbye extends React.Component {
+	render() {
+		return <div> Goodbye </div>
+	}
+}
+
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<App />
+	</Provider>
+	, document.getElementById('root'));
 registerServiceWorker();
