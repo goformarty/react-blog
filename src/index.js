dc77538ef9dba308aaaf1d2cd89a2660a107a3c7
Marty Stepien <marty.stepien@gmail.com>
2018-06-19 17:07:26 +0100

    added switch component to display home route correctly

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 
@@ -20,8 +20,10 @@ ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
 		<BrowserRouter>
 			<div>
-				<Route path='/' component={PostIndex} />
-				<Route path='/posts/new' component={PostNew} />
+				<Switch>
+					<Route path='/posts/new' component={PostNew} />
+					<Route path='/' component={PostIndex} />
+				</Switch>
 			</div>
 		</BrowserRouter>
 	</Provider>
